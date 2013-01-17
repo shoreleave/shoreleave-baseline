dd5693923010219b2bd395a266f5a15f8bcbf7a9
Paul deGrandis <paul.degrandis@gmail.com>
2013-01-17 12:55:58 -0800

    Scrubbed all unnecessary client code

--- src/baseline/client/main.cljs
+++ src/baseline/client/main.cljs
@@ -1,23 +1,2 @@
-(ns baseline.client.main
-  (:require [shoreleave.common :as common]
-            [shoreleave.browser.history :as history])
-  (:require-macros  [shoreleave.remotes.macros :as srm]))
-
-(def query-args (common/query-args-map))
-(def hash-args (common/hash-args-map))
-
-;; Integrate history/back-button to the search
-;For example:
-;    (history/navigate-callback #(process-search (subs (:token %) 4) false))
-;Where process-search is the main action to take when processing the page/url
-
-;; ### Browser REPL
-;; If you add a `repl` as a query-string arg, even on the live Barker,
-;; You can remotely interact with the site from the local REPL
-;; Visit: `http://127.0.0.1:8080/search?repl=yes#q=sat`
-(common/toggle-brepl query-args :repl)
-
-;; ### Confirm we have remote-calling activated
-(srm/rpc (api/ping-the-api "Testing...") [pong-response]
-  (js/alert pong-response))
+(ns baseline.client.main)
 
