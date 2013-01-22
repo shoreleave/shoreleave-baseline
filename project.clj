528ba9aff8b9cb6daaee5718b6340349e9fbabf9
Paul deGrandis <paul.degrandis@gmail.com>
2013-01-21 16:08:57 -0800

    Added the lein min version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0" 
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
                  [compojure "1.1.5" :exclusions [org.clojure/clojure]]
                  [lib-noir "0.3.4" :exclusions [[org.clojure/clojure]
