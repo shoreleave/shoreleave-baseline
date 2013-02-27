9bde0b968c292ab0f22504c6d2fcf8735ee9b2ad
Paul deGrandis <paul.degrandis@gmail.com>
2013-02-27 12:03:46 -0800

    Project updates and safe-read that's actually safe

--- project.clj
+++ project.clj
@@ -1,26 +1,26 @@
 (defproject shoreleave-baseline "0.3.0"
-  :description "A baseline application to get started with Compojure+Shoreleave"
+  :description "A baseline application to get started with a Compojure+Shoreleave"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+                 [org.clojure/tools.reader "0.7.0"]
                  [compojure "1.1.5" :exclusions [org.clojure/clojure]]
-                 [lib-noir "0.3.4" :exclusions [[org.clojure/clojure]
+                 [lib-noir "0.4.7" :exclusions [[org.clojure/clojure]
                                                 [compojure]
                                                 [hiccup]
                                                 [ring]]]
-                 [ring "1.1.7"]
-                 [ring-server "0.2.5" :exclusions [[org.clojure/clojure]
+                 [ring "1.1.8"]
+                 [ring-server "0.2.8" :exclusions [[org.clojure/clojure]
                                                    [ring]]]
-                 [ring-refresh "0.1.1" :exclusions [[org.clojure/clojure]
+                 [ring-refresh "0.1.2" :exclusions [[org.clojure/clojure]
                                                     [compojure]]]
                  [crypto-random "1.1.0"]
                  [amalloy/ring-gzip-middleware "0.1.2" :exclusions [org.clojure/clojure]]
                  [hiccup "1.0.2" :exclusions [org.clojure/clojure]]]
   :dev-dependencies [[lein-marginalia "0.7.1"]]
-  :plugins [[lein-ring "0.8.0" :exclusions [org.clojure/clojure]]
-            [lein-cljsbuild "0.2.10"]]
+  :plugins [[lein-ring "0.8.3" :exclusions [org.clojure/clojure]]
+            [lein-cljsbuild "0.3.0"]]
   :ring {:handler baseline.handler/war-handler
          :init baseline.handler/init
          :destroy baseline.handler/destroy}
